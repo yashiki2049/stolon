5f855ed25567e504aa86de0a420e91a347bfc1c0
yashiki2049 <poppy.red.2003@gmail.com>
2021-01-17 13:08:03 +0900

    ユーザー登録のバリデーション仮設定 他
    ・ユーザー登録のバリデーション設定
    ・それに伴い文字の大きさなど変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は英字と数字の混合で設定してください' }
+         validates :name,     length: { maximum: 8 , message: 'は8文字以内で入力してください' }
+         validates :text,     length: { maximum: 100 , message: 'は100文字以内で入力してください' }
+
          with_options presence: true do
           validates :name
           validates :text
